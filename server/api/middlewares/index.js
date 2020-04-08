716470451533aa27c9a811e776c2b2ccb3d0dd85
michael <evtushenkomisha21@gmail.com>
2020-04-08 21:57:29 +0300

    fix: fixed cors

--- server/api/middlewares/index.js
+++ server/api/middlewares/index.js
@@ -1,10 +1,12 @@
 const bodyParser = require('koa-bodyparser');
 const logger = require('koa-logger');
 const {userAgent} = require('koa-useragent');
+const cors = require('@koa/cors');
 
 const errorHandlerMiddleware = require('./error-handler.middleware');
 
 module.exports = app => {
+    app.use(cors());
     app.use(userAgent);
     app.use(logger());
     app.use(bodyParser());
