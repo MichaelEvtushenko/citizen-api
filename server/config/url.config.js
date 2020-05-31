438e504b7d9d2f9b34aae1868a5b92ba51d3f023
michael <evtushenkomisha21@gmail.com>
2020-06-01 02:35:28 +0300

    refactor: remove unused code and comments

--- server/config/url.config.js
+++ server/config/url.config.js
@@ -1,9 +1,7 @@
 const HOST_NAME = (process.env.HOST_NAME || 'http://localhost') + `:${process.env.PORT || 3000}`;
 
-const API_ALERTS_URL = `${HOST_NAME}/api/alerts`;
 const API_ACTIVATE_URL = `${HOST_NAME}/api/auth/activate`;
 
 module.exports = {
-    API_ALERTS_URL,
     API_ACTIVATE_URL,
 };
