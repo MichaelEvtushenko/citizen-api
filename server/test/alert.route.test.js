d9e4449b8d39af449507cecf7a9d0d21f0352b47
michael <evtushenkomisha21@gmail.com>
2020-06-01 02:24:04 +0300

    test(alerts): Add main test cases

--- server/test/alert.route.test.js
+++ server/test/alert.route.test.js
@@ -4,21 +4,21 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../server');
 const knex = require('../data/db/connection');
-const {shouldHaveProperties} = require('../helpers/test.helper');
+const {shouldHaveProperties} = require('../helpers/test/should.helper');
+const {get, post} = require('../helpers/test/request-helper')(server);
 
 chai.should();
 chai.use(chaiHttp);
 
-before('truncate table', async () => {
+before(async () => {
     await knex.schema.raw('TRUNCATE alerts CASCADE');
-    console.log('before')
 });
 
-beforeEach('get access token', done => {
+beforeEach(done => {
     chai.request(server)
         .post('/api/auth/login')
         .send({
-            email: process.env.TEST_EMAIL || 'xbritschymcq@zaaskater.ml',
+            email: process.env.TEST_EMAIL || 'test@test.abc',
             password: process.env.TEST_PASSWORD || '123123'
         })
         .end((err, res) => {
@@ -28,70 +28,101 @@ beforeEach('get access token', done => {
 });
 
 describe('Alerts', () => {
-    const alertProperties = [
-        'alertId', 'userId', 'latitude', 'longitude', 'description', 'reportedAt', 'status', 'photoUrls'
-    ];
-
-    const alertToCreate = {
-        latitude: 50.44727421482662,
-        longitude: 30.45413374900818,
-        description: 'Someone was shooting near KPI hostel №3'
-    };
 
     describe('POST alerts', () => {
-        it('it should create a new alert, returning ' +
-            'status: 200 OK, ' +
-            `body: { ${alertProperties.join(', ')} }`, done => {
-            chai.request(server)
-                .post('/api/alerts')
-                .set('Authorization', `Bearer ${process.env.testAccessToken}`)
-                .send(alertToCreate)
-                .end((err, res) => {
-                    shouldHaveProperties(alertProperties, res);
-                    done();
+        const alertProperties = [
+            'alertId', 'userId', 'latitude', 'longitude', 'description', 'reportedAt', 'status', 'photoUrls'
+        ];
+
+        const alertToCreate = {
+            latitude: 50.44727421482662,
+            longitude: 30.45413374900818,
+            description: 'Someone was shooting near KPI hostel №3'
+        };
+
+        describe('Validation', () => {
+            it('it should validate "latitude" property',
+                done => {
+                    post('/api/alerts', {...alertToCreate, latitude: null})
+                        .end((err, res) => {
+                            res.should.have.status(400);
+                            res.body.should.have.property('error').equal(true);
+                            res.body.should.have.property('message').equal('Latitude is not valid');
+                            done();
+                        });
+                });
+
+            it('it should validate "longitude" property',
+                done => {
+                    post('/api/alerts', {...alertToCreate, longitude: []})
+                        .end((err, res) => {
+                            res.should.have.status(400);
+                            res.body.should.have.property('error').equal(true);
+                            res.body.should.have.property('message').equal('Longitude is not valid');
+                            done();
+                        });
+                });
+
+            it('it should validate "description" property',
+                done => {
+                    post('/api/alerts', {...alertToCreate, description: {}})
+                        .end((err, res) => {
+                            res.should.have.status(400);
+                            res.body.should.have.property('error').equal(true);
+                            res.body.should.have.property('message').equal('Description is not valid');
+                            done();
+                        });
                 });
         });
+
+        it('it should create a new alert',
+            done => {
+                post('/api/alerts', alertToCreate)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        shouldHaveProperties(alertProperties, res.body);
+                        done();
+                    });
+            });
     });
 
     describe('GET alerts', () => {
-        it('it should validate request params, returning ' +
-            'status: 400 Bad Request, ' +
-            'body: { error: true, message: "Location is not valid"}', done => {
-            chai.request(server)
-                .get('/api/alerts')
-                .end((err, res) => {
-                    res.should.have.status(400);
-                    res.body.should.have.property('error').equal(true);
-                    res.body.should.have.property('message').equal('Location is not valid');
-                    done();
+
+        describe('Validation', () => {
+            it('it should validate query string',
+                done => {
+                    get('/api/alerts')
+                        .end((err, res) => {
+                            res.should.have.status(400);
+                            res.body.should.have.property('error').equal(true);
+                            res.body.should.have.property('message').equal('Location is not valid');
+                            done();
+                        });
+                });
+
+            it('it should validate request parameters',
+                done => {
+                    get('/api/alerts/#')
+                        .end((err, res) => {
+                            res.should.have.status(400);
+                            res.body.should.have.property('error').equal(true);
+                            res.body.should.have.property('message').equal('Location is not valid');
+                            done();
+                        });
                 });
         });
 
-        it('it should validate path params, returning ' +
-            'status: 400 Bad Request, ' +
-            `body: { error: true, message: "Id is not valid"}`, done => {
-            chai.request(server)
-                .get('/api/alerts/@_@')
-                .end((err, res) => {
-                    res.should.have.status(400);
-                    res.body.should.have.property('error').equal(true);
-                    res.body.should.have.property('message').equal('Id is not valid');
-                    done();
-                });
-        });
-
-        it('it should find alerts in radius, returning ' +
-            'status: 200 OK, ' +
-            `body: { alerts: [], radius: 500, unit: "m", limit: 2 }`, done => {
-            chai.request(server)
-                .get('/api/alerts?lat=50.44727421482662&lng=30.45413374900818&limit=2')
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.have.property('alerts');
-                    res.body.should.have.property('radius').equal(500);
-                    res.body.should.have.property('unit').equal('m');
-                    res.body.should.have.property('limit').equal('2');
-                    done();
-                });
-        });
-    })});
+        it('it should find alerts in radius',
+            done => {
+                get('/api/alerts?lat=42.42&lng=20.20&limit=1')
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.have.property('alerts');
+                        res.body.should.have.property('radius').equal(500); // set by default
+                        res.body.should.have.property('unit').equal('m');   // set by default
+                        res.body.should.have.property('limit').equal('1');
+                        done();
+                    });
+            });
+    });
+});
